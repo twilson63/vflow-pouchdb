cbee68196bf265e7f74fcbbdd87ae9149a662b75
Tom Wilson <tom@jackhq.com>
2015-02-14 22:31:09 -0500

    made some adjustmest with security

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ ee = module.exports = (ee = global.ee) != null ? ee : new EventEmitter();
 
 var pouchdb = require('pouchdb');
 var stream = pouchdb(process.env.Vflow || 'vflow');
+var username = null;
 
 stream.changes({
   include_docs: true,
   since: 'now',
   live: true
 }).on('change', function(change) {
+  if (!change.doc.object) return; 
+  if (!change.doc.object.type) return;
   var key = [change.doc.object.type, change.doc.verb].join(':'); 
-  ee.emit(key, change.doc);
+  
+  //console.log(key);
+  if (key === 'user:create' 
+    || key === 'session:create'
+    || key === 'user:created') {
+    return ee.emit(key, change.doc);
+  }
+  if (change.doc.actor && change.doc.actor.name === username) {
+    ee.emit(key, change.doc);
+  }
 });
 
 // always create new event on stream
 ee.on('post', function(p) {
   //console.log(p);
+  if (p.actor && p.actor.name) username = p.actor.name;
+
   stream.post(p);
 });
