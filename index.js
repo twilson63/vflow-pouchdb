6aad0299e84cea9bf0c6d47b1f05b427fe888fc4
Tom Wilson <tom@jackhq.com>
2015-02-15 17:06:55 -0500

    refactor filter

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ ee = module.exports = (ee = global.ee) != null ? ee : new EventEmitter();
 var pouchdb = require('pouchdb');
 var stream = pouchdb(process.env.Vflow || 'vflow');
 var uuid = require('node-uuid');
-
-var username = null;
 var systemId = uuid.v4();
 
 stream.changes({
@@ -18,21 +16,11 @@ stream.changes({
   var key = [change.doc.object.type, change.doc.verb].join(':'); 
   
   //console.log(key);
-  if (key === 'user:create' 
-    || key === 'session:create'
-    || key === 'user:created'
-    || key === 'session:isactive'
-    || key === 'session:inactive') {
+  if (['profile:create','profile:update','profile:remove'].indexOf(key) > -1) {
     return ee.emit(key, change.doc);
   }
-  if (change.doc.verb === 'create' 
-    || change.doc.verb === 'update'
-    || change.doc.verb === 'remove') {
-    return ee.emit(key, change.doc);
-  }
-  if (change.doc.actor 
-    && change.doc.actor.name === username
-    && change.doc.systemId
+
+  if (change.doc.systemId
     && change.doc.systemId === systemId) {
     ee.emit(key, change.doc);
   }
@@ -41,7 +29,6 @@ stream.changes({
 // always create new event on stream
 ee.on('post', function(p) {
   p.systemId = systemId;
-  if (p.actor && p.actor.name) username = p.actor.name;
 
   stream.post(p);
 });
